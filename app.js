f9fda4b42ceb6e8df1f35e342131d843a6dd26b2
bretw84 <bretw84@gmail.com>
2018-01-31 13:57:19 -0600

    Still cant figure stuff out, committing for more help from dean

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var game = new Game(3);
 
 for(var i=1; i < game.numOfPlayers+1; i++) {
-	console.log(i);
-   game.players['player'+i] = new Player("player"+i)
+    console.log(i);
+    game.players['player'+i] = new Player("player"+i);
 }
+
+game.players['player1'].q1 = 14;
 
 console.log(game.players['player1'].score);
--- player.js
+++ player.js
@@ -4,7 +4,7 @@ class Player {
 
         this.name=name;
         this.q1=0;
-        this.q2=7;
+        this.q2=0;
         this.q3=0;
         this.q4=0;
         this.score=this.q1+this.q2+this.q3+this.q4;
