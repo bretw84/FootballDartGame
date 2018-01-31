076a08e16e368aef3d26a1ea30841f678daff078
bjtwech <bretw84@gmail.com>
2018-01-31 08:49:20 -0600

    Update player.js

--- player.js
+++ player.js
@@ -3,12 +3,19 @@ class Player {
     constructor(name) {
 
         this.name=name;
-        this.q1='--';
-        this.q2='--';
-        this.q3='--';
-        this.q4='--';
-        this.score=0;
+        this.q1=0;
+        this.q2=7;
+        this.q3=0;
+        this.q4=0;
+        this.score=this.q1+this.q2+this.q3+this.q4;
 
     };//END constructor
 
+	
+	addScore()
+	{
+		
+	}
+	
+	
 }//END Player
