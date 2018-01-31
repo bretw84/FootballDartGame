3069cdf171c621897543879fa9845e877924d0f9
bjtwech <bretw84@gmail.com>
2018-01-31 08:47:48 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var game = new Game(3);
 
-for(var i=1; i < game.players; i++) {
-    console.log(i);
+for(var i=1; i < game.numOfPlayers+1; i++) {
+	console.log(i);
+   game.players['player'+i] = new Player("player"+i)
 }
 
-console.log("Num Players"+game.players);
+console.log(game.players['player1'].score);
