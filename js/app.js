14109f29c2a4a7f72723a9e0c8263784f17cc041
bretw84 <bretw84@gmail.com>
2018-02-03 08:51:33 -0600

    Almost working.. one bug when it changes quarters not printing points in the table cell

--- js/app.js
+++ js/app.js
@@ -1,68 +1,237 @@
 $(document).ready(function() {
-	
-    var game = 0;
-    var quarter = 1;
+    
+    /**
+     * Main game variable ends up being an object. not sure how that works
+     */
+    var game;
+
+    /**
+     * 
+     * current players turn variable
+     * gets incremented on turnButton click
+     * when turn = number of players sets back to 1
+     * and quarter increments
+     * 
+     */
     var turn = 1;
 
+    /**
+     * 
+     * current quarter variable.
+     * gets incremented when turns = number of players
+     * button appears that restarts game after variable = 4 (4 quarters)
+     *  
+     */
+    var quarter = 1;
+
+
+    /**
+     * 
+     * newGame button click event
+     * hides newGmae button
+     * shows numPlayersBox
+     * 
+     */
     $('#newGame').click(function() {
+
+        console.log('New game button was clicked.')
+
         $(this).addClass('hide');
         $('#numPlayersBox').removeClass('hide');
+
+        $('#numPlayers').focus();
+
     });//END #newGame click function
 
+    /**
+     * 
+     * #numPlayersButton click event
+     * validates input
+     * successful input calls new Game object
+     * which gets passed value from numPlayers 
+     * and becomes game.numPlayers
+     * 
+     * loops through game.numPlayers and creates html input boxes to enter each players name
+     * 
+     * @html #player*Box 
+     * 
+     * 
+     */
     $('#numPlayersButton').click(function() {
+
+        console.log('Number of player button was clicked');
     
+        //assign input to numPlayers
         var numPlayers = $('#numPlayers').val();
 
+        console.log('Number of players entered: '+numPlayers);
+
+        //check if numPlayers is empty, 0 or not a number
         if(numPlayers == '' || numPlayers == 0 || isNaN(numPlayers)) {
+
+            console.log('ERROR: Number of players entered was empty, zero, or not a number')
 
             alert('You must have at least 1 player to continue');
             $('#numPlayers').focus().css('border-color', 'red');
             return;
 
+        //check if numPlayer more than 8
         } else if(numPlayers > 8) {
+
+            console.log("ERROR: Number of players entered was greater than 8");
 
             alert('There can only be a max of 8 players.');
             $('#numPlayers').focus().css('border-color', 'red');
             return;
 
+        //succesful numPlayers create new Game object and assign game.numPlayers
         } else {
 
             $('#numPlayersBox').addClass('hide');
             $('#playerDetails').removeClass('hide');
+
+            //create game object and assign numPlayers
             game = new Game(numPlayers);
-			
+            
+            // loop through game.numPlayers and create html input boxes to enter each players name
 			for(var i=1; i<parseInt(game.numPlayers)+1; i++) {
 				$('#playersRow').append('<div id="player'+i+'Box" class="col-md-3"><label>Player '+i+' Name</label><div><input type="text" id="player'+i+'" class="form-control" placeholder="Enter Player '+i+' Name" /></div></div>');
-			}
+            }
+            
+            console.log('Pending game with '+game.numPlayers+' players is ready for names.');
+
+            $('#player1').focus();
 
         }//END validation
 
     });//END numPlayersButton click funcion
 
+    /**
+     * 
+     * startGame click event 
+     * 
+     * 
+     */
 	$('#startGame').click(function() {
 
 		$('#playerDetails').addClass('hide');
-		$('#boxScore,#mainGame').removeClass('hide');
+        $('#boxScore,#mainGame').removeClass('hide');
 
 		for(var i=1; i<parseInt(game.numPlayers)+1; i++) {
+
 			game.player['player'+i] = new Player(i,"player"+i);
-			game.player['player'+i].name = $('#player'+i).val();
-			$('#scoreBoard tbody').append('<tr id="player'+i+'Row"><td>'+game.player['player'+i].name+'</td><td id="player'+i+'q1">--</td><td id="player'+i+'q2">--</td><td id="player'+i+'q3">--</td><td id="player'+i+'q4">--</td><td id="player'+i+'score">0</td></tr>');
+            game.player['player'+i].name = $('#player'+i).val();
+
+            console.log("Player "+i+" is "+game.player['player'+i].name);
+
+			$('#scoreBoard tbody').append('<tr id="player'+i+'Row"><td>'+game.player['player'+i].name+'</td><td id="player'+i+'q1">'+game.player['player'+i].q1+'</td><td id="player'+i+'q2">'+game.player['player'+i].q2+'</td><td id="player'+i+'q3">'+game.player['player'+i].q3+'</td><td id="player'+i+'q4">'+game.player['player'+i].q4+'</td><td id="player'+i+'score">'+game.player['player'+i].score+'</td></tr>');
         }
+
+        $('#playerCardHeading').html("Enter " + game.player['player'+turn].name + "'s Quarter "+quarter+" Score.");
+        $('#scoreInput').html('<input type="text" id="player'+turn+'ScoreQ'+quarter+'" class="scoreInput"/>');
+
+        console.log("Names, players and ids assigned. Ready to play!!!");
+
+        console.log('Current Quarter: ' + quarter);
+        console.log('Current Turn: '+game.player['player'+turn].name);
+
+        //console.log('Turn: '+turn);
+        //console.log('quarter: '+quarter);
+
+        $('#turnLabel').html("Current Turn: <strong style='color:green;'>"+ game.player['player'+turn].name+"</strong>");
+        $('#quarterLabel').html('Quarter: ' + quarter);
+        $('.scoreInput').focus();
         
+
+        /**
+         * 
+         * turnButton click event
+         * 
+         * 
+         * 
+         */
         $('#turnButton').click(function() {
-            turn++;
-			if(turn == game.numPlayers) {
+
+            console.log('#turnButton clicked');
+
+            game.player['player'+turn].addPoints(quarter,parseInt($('#player'+turn+'ScoreQ'+quarter).val()));
+            $('#scoreInput').html('<input type="text" id="player'+turn+'ScoreQ'+quarter+'" class="scoreInput"/>');
+            console.log(game.player['player'+turn].name+' current total score is '+game.player['player'+turn].score);
+
+            switch(turn) {
+                case 1:
+                $('#player'+turn+'q'+quarter).html(game.player['player'+turn].q1);
+                $('#player'+turn+'score').html(game.player['player'+turn].score);
+                console.log('#player'+turn+'q'+quarter);
+                console.log('#player'+turn+'score');
+                break;
+
+                case 2:
+                $('#player'+turn+'q'+quarter).html(game.player['player'+turn].q2);
+                $('#player'+turn+'score').html(game.player['player'+turn].score);
+                console.log('#player'+turn+'q'+quarter);
+                console.log('#player'+turn+'score');
+                break;
+
+                case 3:
+                $('#player'+turn+'q'+quarter).html(game.player['player'+turn].q3);
+                $('#player'+turn+'score').html(game.player['player'+turn].score);
+                console.log('#player'+turn+'q'+quarter);
+                console.log('#player'+turn+'score');
+                break;
+
+                case 4:
+                $('#player'+turn+'q'+quarter).html(game.player['player'+turn].q4);
+                $('#player'+turn+'score').html(game.player['player'+turn].score);
+                console.log('#player'+turn+'q'+quarter);
+                console.log('#player'+turn+'score');
+                break;
+            }
+
+            ++turn;
+
+            if(turn-1 == game.numPlayers) {
+
+                if(quarter == 5) {
+                    console.log('Game Over');
+                    $('#mainGame').html('<h1>And the winner is...</h1>');
+                    $('#resetGame').removeClass('hide');
+                }
+                
 				turn = 1;
-				quarter++;
+                quarter++;
+
+                // console.log('Current Quarter: ' + quarter);
+                // console.log('Current Turn: '+game.player['player'+turn].name);
 			}
-			if(quarter == 4) {
-				turn=1;
-				quarter=1;
-			}
-            console.log('Turn: '+turn);
-            console.log('quarter: '+quarter);
-        });
+
+            $('#turnLabel').html("Current Turn: <strong style='color:green;'>"+ game.player['player'+turn].name+"</strong>");
+            $('#quarterLabel').html('Quarter: ' + quarter);
+
+            $('#playerCardHeading').html("Enter " + game.player['player'+turn].name + "'s Quarter "+quarter+" Score.");
+            $('#scoreInput').html('<input type="text" id="player'+turn+'ScoreQ'+quarter+'" class="scoreInput"/>');
+            $('.scoreInput').focus();
+			
+			if(quarter == 5) {
+                console.log('Game Over');
+                $('#mainGame').html('<h1>And the winner is...</h1>');
+                $('#resetGame').removeClass('hide');
+            }
+            
+            // console.log('Current Quarter: ' + quarter);
+            // console.log('Current Turn: '+game.player['player'+turn].name);
+
+            /**
+             * 
+             * #resetButton click event
+             * reloads window and starts game over
+             * 
+             */
+            $('#resetGame').click(function(){
+                window.location.reload();
+            });//END resetButton click function
+
+        });//END turnButton click function
 
 	});//END startGame click function
 
